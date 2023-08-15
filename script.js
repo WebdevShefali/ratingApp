b2bf577d4130810162447dace9760caf45177dd5
WebdevShefali <jangidshefali62@gmail.com>
2023-08-15 12:42:46 +0530

    Update styles

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ let giphy = document.querySelector(".giphy");
 let btn = document.querySelector(".btn");
 let info = document.querySelector(".info");
 
-for(var i=0; i<emoji.length; i++){
-  emoji[i].addEventListener("click",function(){
-    heading.innerHTML = "Thanks for giving your time!"
-    giphy.style.visibility = "visible"
-    btn.style.visibility = "visible"
-    emojiSection.style.fontSize = "20px"
-    emojiSection.style.padding = "0px"
-      emojiSection.innerHTML = ""
-info.style.visibility = "visible"
-header.style.padding = "30px"
-heading.style.paddingBottom = "5px"
-});
-};
+for (var i = 0; i < emoji.length; i++) {
+  emoji[i].addEventListener("click", function () {
+    heading.innerHTML = "Thanks for giving your time!";
+    giphy.style.visibility = "visible";
+    btn.style.visibility = "visible";
+    emojiSection.style.fontSize = "20px";
+    emojiSection.style.padding = "0px";
+    emojiSection.innerHTML = "";
+    info.style.visibility = "visible";
+    header.style.padding = "30px";
+    heading.style.paddingBottom = "5px";
+  });
+}
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function () {
   location.href = "index.html";
 });
